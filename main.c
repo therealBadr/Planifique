3dfbaedf6c2a85e7dc3d0837b55ebfdc0b4b2d8b
Badr El Abdellaoui <b.elabdellaoui101@gmail.com>
2024-01-13 17:32:01 +0100

    Create account window

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 #include <string.h>
 #include <gtk/gtk.h>
 
+// Function prototypes
+void on_login_button_clicked(GtkWidget *widget, gpointer data);
+void on_create_account_button_clicked(GtkWidget *widget, gpointer data);
+void on_create_button_clicked(GtkWidget *widget, gpointer data);
+
+// Widgets for the main window
 GtkWidget *username_entry;
 GtkWidget *password_entry;
+
+// Widgets for the account creation window
+GtkWidget *create_account_window;
+GtkWidget *create_username_entry;
+GtkWidget *create_password_entry;
+GtkWidget *create_button;
 
 // Function to handle login button click
 void on_login_button_clicked(GtkWidget *widget, gpointer data) {
@@ -19,9 +31,92 @@ void on_login_button_clicked(GtkWidget *widget, gpointer data) {
 
 // Function to handle create account button click
 void on_create_account_button_clicked(GtkWidget *widget, gpointer data) {
-    // Add logic to open the "Create Account" window or perform other actions
-    g_print("Opening Create Account window...\n");
+    // Create a new window for account creation
+    create_account_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(create_account_window), "Create Account");
+    gtk_window_set_default_size(GTK_WINDOW(create_account_window), 400, 200);
+
+    // Vertical box to hold widgets in the account creation window
+    GtkWidget *create_account_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+    gtk_container_add(GTK_CONTAINER(create_account_window), create_account_box);
+
+    // Username field in account creation window
+    GtkWidget *create_username_label = gtk_label_new("Enter Username:");
+    gtk_box_pack_start(GTK_BOX(create_account_box), create_username_label, FALSE, FALSE, 10);
+    create_username_entry = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(create_account_box), create_username_entry, FALSE, FALSE, 5);
+
+    // Password field in account creation window
+    GtkWidget *create_password_label = gtk_label_new("Enter Password:");
+    gtk_box_pack_start(GTK_BOX(create_account_box), create_password_label, FALSE, FALSE, 5);
+    create_password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(create_password_entry), FALSE); // Hide password
+    gtk_box_pack_start(GTK_BOX(create_account_box), create_password_entry, FALSE, FALSE, 5);
+
+    // Create button in account creation window
+    create_button = gtk_button_new_with_label("Create Account");
+    gtk_box_pack_start(GTK_BOX(create_account_box), create_button, FALSE, FALSE, 10);
+
+    // Connect "clicked" signal to create account function
+    g_signal_connect(create_button, "clicked", G_CALLBACK(on_create_button_clicked), NULL);
+
+    // Show all widgets in the account creation window
+    gtk_widget_show_all(create_account_window);
 }
+
+// Function to handle create button click in account creation window
+void on_create_button_clicked(GtkWidget *widget, gpointer data) {
+    const gchar *create_username = gtk_entry_get_text(GTK_ENTRY(create_username_entry));
+    const gchar *create_password = gtk_entry_get_text(GTK_ENTRY(create_password_entry));
+
+    // Check if a file with the same username already exists
+    char filename[50];
+    sprintf(filename, "%s.txt", create_username);
+
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        // Close the file if it exists
+        fclose(file);
+
+        // Display a pop-up for an existing account
+        GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(create_account_window),
+                                                   GTK_DIALOG_MODAL,
+                                                   GTK_MESSAGE_ERROR,
+                                                   GTK_BUTTONS_OK,
+                                                   "An account with the same username already exists.");
+        gtk_dialog_run(GTK_DIALOG(dialog));
+        gtk_widget_destroy(dialog);
+    } else {
+        // Create a new file with username and password as its name
+        file = fopen(filename, "w");
+        if (file != NULL) {
+            fprintf(file, "Username: %s\nPassword: %s\n", create_username, create_password);
+            fclose(file);
+
+            // Display a pop-up for a successfully created account
+            GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(create_account_window),
+                                                       GTK_DIALOG_MODAL,
+                                                       GTK_MESSAGE_INFO,
+                                                       GTK_BUTTONS_OK,
+                                                       "Account created successfully!");
+            gtk_dialog_run(GTK_DIALOG(dialog));
+            gtk_widget_destroy(dialog);
+
+            // Close the account creation window
+            gtk_widget_destroy(create_account_window);
+        } else {
+            // Display a pop-up for an error in creating the account file
+            GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(create_account_window),
+                                                       GTK_DIALOG_MODAL,
+                                                       GTK_MESSAGE_ERROR,
+                                                       GTK_BUTTONS_OK,
+                                                       "Error creating the account file.");
+            gtk_dialog_run(GTK_DIALOG(dialog));
+            gtk_widget_destroy(dialog);
+        }
+    }
+}
+
 
 int main(int argc, char *argv[]) {
     GtkWidget *window;
@@ -30,7 +125,7 @@ int main(int argc, char *argv[]) {
     GtkWidget *username_label;
     GtkWidget *password_label;
     GtkWidget *login_button;
-    GtkWidget *create_account_button;  // Replace create_account_link with a button
+    GtkWidget *create_account_button;
 
     // Initializing GTK
     gtk_init(&argc, &argv);
@@ -38,7 +133,7 @@ int main(int argc, char *argv[]) {
     // Creating a window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Planifique - Login");
-    gtk_window_set_default_size(GTK_WINDOW(window),600, 400);
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
     // Vertical box to hold widgets in the main window
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
@@ -67,7 +162,7 @@ int main(int argc, char *argv[]) {
 
     // Create account button
     create_account_button = gtk_button_new_with_label("Create an account");
-    gtk_box_pack_start(GTK_BOX(main_box), create_account_button, FALSE, FALSE, 5);
+    gtk_box_pack_start(GTK_BOX(main_box), create_account_button, FALSE, FALSE, 10);
 
     // Connect "clicked" signals to respective functions
     g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_button_clicked), NULL);
